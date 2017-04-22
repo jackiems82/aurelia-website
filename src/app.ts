d41b73d9ed1506adb4c41afde947215021701cbd
jack <jackie.ms@gmx.de>
2017-04-22 21:20:03 +0200

    added logo to nav

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ export class App {
 
     config.map([
       { route: ['', 'home'],  title: 'Home',        name: 'home',         moduleId: 'home' },
-      { route: 'projects',    title: 'Projects',    name: 'projects',     moduleId: 'projects/projects' },
+      { route: 'projects',    title: 'Projects',    name: 'projects',     moduleId: 'projects/projects', nav:true },
       { route: 'contact',     title: 'Contact',     name: 'contact',      moduleId: 'misc/contact', nav:true },
       { route: 'impressum',   title: 'Impressum',   name: 'impressum',    moduleId: 'misc/impressum.html', nav:true },
       { route: 'disclaimer',  title: 'Disclaimer',  name: 'disclaimer',   moduleId: 'misc/disclaimer.html', nav:true }
