e71cd10854a49d894e4e8122019d77239588f024
jack <jackie.ms@gmx.de>
2017-04-25 00:25:32 +0200

    style changes, refactoring - home screen almost complete, image not shown in FF and IE

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,10 @@ export class App {
     config.title = '3Boards';
 
     config.map([
-      { route: ['', 'home'],  title: 'Home',        name: 'home',         moduleId: 'home' },
-      { route: 'projects',    title: 'Projects',    name: 'projects',     moduleId: 'projects/projects', nav:true },
-      { route: 'contact',     title: 'Contact',     name: 'contact',      moduleId: 'misc/contact', nav:true },
+      { route: ['', 'home'],  title: 'Home',        name: 'home',         moduleId: 'pages/home.html' },
+      { route: 'projects',    title: 'Projects',    name: 'projects',     moduleId: 'pages/projects', nav:true },
+      { route: 'about',       title: 'About',       name: 'about',        moduleId: 'pages/about.html', nav:true },
+      { route: 'contact',     title: 'Contact',     name: 'contact',      moduleId: 'misc/contact' },
       { route: 'impressum',   title: 'Impressum',   name: 'impressum',    moduleId: 'misc/impressum.html', nav:true },
       { route: 'disclaimer',  title: 'Disclaimer',  name: 'disclaimer',   moduleId: 'misc/disclaimer.html', nav:true }
     ]);
--- src/web-api.ts
+++ src/web-api.ts
@@ -49,4 +49,22 @@ let data:BoardDetails[] =
             {imageUrl:"https://abload.de/img/17097873_10208962076263bxz.jpg", thumb:""},
             {imageUrl:"https://abload.de/img/img_5627juaw1.jpg", thumb:""}            
         ]
+    },{
+        description: { header: "Board1" , text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus at eros sit amet mattis. Nullam sed massa velit. Aliquam non erat nec metus egestas eleifend et a orci. Quisque justo tellus, posuere id pellentesque id, euismod non nibh. Proin ut pellentesque mauris. Suspendisse potenti. Proin quis rhoncus est."
+
++ "Suspendisse rhoncus nisl id dolor faucibus, pharetra pretium mi ornare. Maecenas rhoncus laoreet nulla, sit amet pharetra risus aliquet nec. Phasellus nec sagittis arcu, eu porttitor justo. Nulla vitae mi auctor nunc egestas tempus ut aliquam nunc. Nunc sem ex, condimentum eu elementum vitae, egestas imperdiet enim. Phasellus maximus erat ut mauris commodo fermentum. Ut ut erat ac dui egestas efficitur. Phasellus non maximus massa. Integer vitae ornare nisl. Curabitur auctor felis enim, eu commodo diam volutpat eu. Vestibulum eget orci sed elit pretium congue eget et tortor. Aenean sed egestas eros. In id arcu a est viverra laoreet at sit amet dui. Nullam euismod venenatis tempus." },
+        images: [
+            {imageUrl:"https://abload.de/img/17097873_10208962076263bxz.jpg", thumb:""},
+            {imageUrl:"https://abload.de/img/img_5627juaw1.jpg", thumb:""},
+            {imageUrl:"https://abload.de/img/dscf0257hrshj.jpg", thumb:""}
+        ]
+    },{
+        description: { header: "Board1" , text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus at eros sit amet mattis. Nullam sed massa velit. Aliquam non erat nec metus egestas eleifend et a orci. Quisque justo tellus, posuere id pellentesque id, euismod non nibh. Proin ut pellentesque mauris. Suspendisse potenti. Proin quis rhoncus est."
+
++ "Suspendisse rhoncus nisl id dolor faucibus, pharetra pretium mi ornare. Maecenas rhoncus laoreet nulla, sit amet pharetra risus aliquet nec. Phasellus nec sagittis arcu, eu porttitor justo. Nulla vitae mi auctor nunc egestas tempus ut aliquam nunc. Nunc sem ex, condimentum eu elementum vitae, egestas imperdiet enim. Phasellus maximus erat ut mauris commodo fermentum. Ut ut erat ac dui egestas efficitur. Phasellus non maximus massa. Integer vitae ornare nisl. Curabitur auctor felis enim, eu commodo diam volutpat eu. Vestibulum eget orci sed elit pretium congue eget et tortor. Aenean sed egestas eros. In id arcu a est viverra laoreet at sit amet dui. Nullam euismod venenatis tempus." },
+        images: [
+            {imageUrl:"https://abload.de/img/17097873_10208962076263bxz.jpg", thumb:""},
+            {imageUrl:"https://abload.de/img/img_5627juaw1.jpg", thumb:""},
+            {imageUrl:"https://abload.de/img/dscf0257hrshj.jpg", thumb:""}
+        ]
     }];
